4d2491fbe9b8172e9059a03aa3ba7e93f7a6af87
Fito <fito@paragil.com>
2013-05-04 15:02:37 -0700

    initial commit

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, "AiTpQkPMgLcLA3VTIE3Rpw", "DORT9zRTDaDJw2yNXl2QMvPIPFeCQ7IUjd7Hqzh2Kw"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wunderkam::Application.config.secret_token = '0cbcffe024daacba5c3fc0b23c7054058506aa52f08d56b518a7b419b2278fc5e652495029f5769bbc552525b8226494f4a71277d22e159f733ce6f4dcaa9115'
